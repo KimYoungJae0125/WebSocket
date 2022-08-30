3f7caf4990364dc076fbbfc59144f71fd558765d
KimYoungJae <uni0125@naver.com>
2022-08-31 00:18:58 +0900

    Update Cypress
    index.html 에서 cypress 관련 의존성 분리

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,11 +1,18 @@
 describe('Chatting Test', () => {
     it('1. 화면 접속', () => {
         cy.visit("/index");
+        window.open(location.origin + "/index");
     });
     it('2. 채팅 메시지 입력', () => {
-        cy.get("#msg").type("사이프레스로 채팅");
+        cy.get("[data-cy='messageInput']").type("사이프레스로 채팅");
     });
     it('3. 채팅 메시지 전송', () => {
-        cy.get("#sendButton").click();
+        cy.get("[data-cy='sendButton']").click();
+    });
+    it('4. 채팅 여러번 보내보기', () => {
+        for(let i=0; i<10; i++) {
+            cy.get("[data-cy='messageInput']").type(i);
+            cy.get("[data-cy='sendButton']").click();
+        }
     });
 })
